aaeb6e9800d187ffb0678613eecaad8ce4f04acf
lwy210 <leeoo6436210@gmail.com>
2023-06-08 11:58:23 +0900

    feat:  comment 모델 작성 및 테이블 생성

--- comment/models.py
+++ comment/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+class Post(models.Model):
+    post_id = models.BigIntegerField()
+    user_id = models.BigIntegerField()
+    content = models.CharField(max_length=255)
+    registered_time = models.DateTimeField()
+    updated_time = models.DateTimeField()
+    recommendation = models.IntegerField()
