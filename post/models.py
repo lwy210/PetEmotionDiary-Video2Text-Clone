0d0bc7f72c79fa8a1d27ae0c65f695623e015f3a
lwy210 <leeoo6436210@gmail.com>
2023-06-12 14:30:13 +0900

    feat: 유저와 연관관계 설정

--- post/models.py
+++ post/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 
+from account.models import User
+
 
 # Create your models here.
 class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_post")
     kind = models.CharField(max_length=10)
     category = models.CharField(max_length=10)
     title = models.CharField(max_length=45)
