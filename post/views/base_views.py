0e1e19add3ed33b8d3d8869486747588f628770e
lwy210 <leeoo6436210@gmail.com>
2023-06-12 09:43:46 +0900

    feat: 게시판 게시글 카테고리 기능 구현

--- post/views/base_views.py
+++ post/views/base_views.py
@@ -1,10 +1,21 @@
+from django.db.models import Q
 from django.shortcuts import get_object_or_404, render
 
 from ..models import Post
 
 
 def index(request):
-    post_list = Post.objects.order_by("-registered_time")
+    q = Q()
+    category = request.GET.get("category")
+    kind = request.GET.get("kind")
+
+    if category:
+        q &= Q(category=category)
+
+    if kind:
+        q &= Q(kind=kind)
+
+    post_list = Post.objects.filter(q).order_by("-registered_time")
     context = {"post_list": post_list}
     return render(request, "post/post_list.html", context)
 
