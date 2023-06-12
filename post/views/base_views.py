74696f256e11c8bc6c278be073aaf13680eabb46
lwy210 <leeoo6436210@gmail.com>
2023-06-12 13:13:40 +0900

    feat: 검색 기능 구현

--- post/views/base_views.py
+++ post/views/base_views.py
@@ -7,7 +7,7 @@ from ..models import Post
 
 def index(request):
     page = request.GET.get("page", "1")  # 페이지
-    print(page)
+    kw = request.GET.get("kw", "")  # 검색어
     q = Q()
     category = request.GET.get("category")
     kind = request.GET.get("kind")
@@ -19,9 +19,13 @@ def index(request):
         q &= Q(kind=kind)
 
     post_list = Post.objects.filter(q).order_by("-registered_time")
+    if kw:
+        post_list = post_list.filter(
+            Q(title__icontains=kw) | Q(content__icontains=kw)  # 제목 검색
+        ).distinct()
     paginator = Paginator(post_list, 10)  # 페이지당 10개씩 보여주기
     page_obj = paginator.get_page(page)
-    context = {"post_list": page_obj}
+    context = {"post_list": page_obj, "page": page, "kw": kw}
     return render(request, "post/post_list.html", context)
 
 
