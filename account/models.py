a223441c3bf08a0f1e3463752c669e29a2341f8d
kawksung <alstjd5479@naver.com>
2023-06-12 13:33:20 +0900

    feat: 커스텀 유저 모델 구현

--- account/models.py
+++ account/models.py
@@ -1,3 +1,79 @@
 from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
+from django.utils import timezone
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    def create_user(
+        self,
+        email,
+        birth_day,
+        user_name,
+        nick_name,
+        update_time,
+        password=None,
+    ):
+        if not email:
+            raise ValueError("Users must have an email address")
+
+        user = self.model(
+            email=self.normalize_email(email),
+            birth_day=birth_day,
+            user_name=user_name,
+            nick_name=nick_name,
+            create_time=timezone.now(),
+            update_time=update_time,
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password):
+        user = self.create_user(
+            email,
+            password=password,
+            birth_day=timezone.now(),
+            nick_name="super",
+            user_name="super",
+            update_time=timezone.now(),
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser):
+    email = models.EmailField(
+        verbose_name="email",
+        max_length=45,
+        unique=True,
+    )
+
+    user_name = models.CharField(max_length=15)
+    nick_name = models.CharField(max_length=15)
+
+    birth_day = models.DateField()
+    create_time = models.DateTimeField()
+    update_time = models.DateTimeField()
+
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
+
+    @property
+    def is_staff(self):
+        return self.is_admin
--- config/settings.py
+++ config/settings.py
@@ -85,6 +85,7 @@ DATABASES = {
     }
 }
 
+AUTH_USER_MODEL = "account.User"
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
