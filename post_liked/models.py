b57e859951877c8a17da267323bd84c872619523
lwy210 <leeoo6436210@gmail.com>
2023-06-13 17:27:59 +0900

    feat: PostLiked 모델 작성 및 테이블 생성

--- post_liked/models.py
+++ post_liked/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+from account.models import User
+from post.models import Post
+
+
+class PostLiked(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    is_liked = models.BooleanField(default=True)
