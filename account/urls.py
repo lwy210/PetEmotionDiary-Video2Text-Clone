0a9d54aec28d0e2bf78c9ddb69b66939d5200faf
kawksung <alstjd5479@naver.com>
2023-06-13 13:52:37 +0900

    feat: 로그인 기능 구현

--- account/urls.py
+++ account/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 from .views import signup_views
 
@@ -6,5 +7,9 @@ app_name = "account"
 
 urlpatterns = [
     path("signup/", signup_views.signup, name="signup"),
-    path("login/", signup_views.login1, name="login"),
+    path(
+        "login/",
+        auth_views.LoginView.as_view(template_name="account/login.html"),
+        name="login",
+    ),
 ]
