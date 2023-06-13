65d6be59503ee7441d951368e5a65e9b2726c1b2
lwy210 <leeoo6436210@gmail.com>
2023-06-13 13:35:49 +0900

    fix: 댓글 폼 적용 #22

--- post/views/comment_views.py
+++ post/views/comment_views.py
@@ -1,6 +1,8 @@
+from django.http import HttpResponseNotAllowed
 from django.shortcuts import get_object_or_404, redirect, render
 from django.utils import timezone
 
+from comment.forms import CommentForm
 from comment.models import Comment
 
 from ..models import Post
@@ -8,12 +10,17 @@ from ..models import Post
 
 def comment_create(request, post_id):
     post = get_object_or_404(Post, pk=post_id)
-    answer = Comment(
-        post=post,
-        content=request.POST.get("content"),
-        registered_time=timezone.now(),
-        updated_time=timezone.now(),
-        recommendation=0,
-    )
-    answer.save()
-    return redirect("post:detail", post_id=post.id)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.registered_time = timezone.now()
+            comment.updated_time = timezone.now()
+            comment.recommendation = timezone.now()
+            comment.post = post
+            comment.save()
+            return redirect("post:detail", post_id=post.id)
+    else:
+        return HttpResponseNotAllowed("Only POST is possible.")
+    context = {"post": post, "form": form}
+    return render(request, "post/post_detail.html", context)
