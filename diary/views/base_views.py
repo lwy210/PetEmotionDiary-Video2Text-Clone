d91989daef2a9a2aaeaae7f5abf6ed9fbd8b6dca
lwy210 <leeoo6436210@gmail.com>
2023-06-16 14:53:48 +0900

    feat: 반려동물 카테고리 기능 구현

--- diary/views/base_views.py
+++ diary/views/base_views.py
@@ -1,10 +1,19 @@
+from django.db.models import Q
 from django.shortcuts import render
+
+from pet.models import Pet
 
 from ..models import Diary
 
 
 def index(request):
-    diary_list = Diary.objects.order_by("-day")
+    q = Q()
+    pet_id = request.GET.get("pet_id")
+    if pet_id:
+        q &= Q(pet_id=pet_id)
 
-    context = {"diary_list": diary_list}
+    diary_list = Diary.objects.filter(q).order_by("-day")
+    print(diary_list)
+    pet_list = Pet.objects.order_by("birth_day")
+    context = {"diary_list": diary_list, "pet_list": pet_list}
     return render(request, "diary/diary_list.html", context)
