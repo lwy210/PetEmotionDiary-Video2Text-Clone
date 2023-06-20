4e767d388243d329c9f19adee74a5de76adc52e1
lwy210 <leeoo6436210@gmail.com>
2023-06-20 16:48:48 +0900

    feat: 일기 생성 기능 구현

--- diary/views/diary_views.py
+++ diary/views/diary_views.py
@@ -1,10 +1,66 @@
+import json
 from urllib.parse import urlencode
 
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
-from django.shortcuts import get_object_or_404, redirect, resolve_url
+from django.shortcuts import get_object_or_404, redirect, render, resolve_url
+from django.utils import timezone
 
-from ..models import Diary
+from ..forms import DiaryForm
+from ..models import Diary, Keyword
+
+
+@login_required(login_url="account:login")
+def diary_create_before(request):
+    if request.method == "POST":
+        form = DiaryForm(request.user, request.POST, request.FILES)
+        if form.is_valid():
+            # < -- 모델 처리 -- >
+
+            content_list = request.POST.getlist("content[]")
+            context = {
+                "form": form,
+                "content": "\n".join(content_list[1:]),
+                "title": content_list[0],
+                "keywords": ["즐거움", "행복", "산책"],  # 생성된 keyword
+            }
+
+            return render(request, "diary/diary_result_form.html", context)
+    else:
+        form = DiaryForm(request.user)
+    context = {"form": form}
+    return render(request, "diary/diary_form.html", context)
+
+
+@login_required(login_url="account:login")
+def diary_create(request):
+    if request.method == "POST":
+        form = DiaryForm(request.user, request.POST)
+        if form.is_valid():
+            diary = form.save(commit=False)
+            diary.user = request.user
+            diary.registered_time = timezone.now()
+            diary.updated_time = timezone.now()
+            diary.content = request.POST.get("content")
+            diary.title = request.POST.get("title")
+
+            checked_list_str = request.POST["keyword_list"]
+            checked_list = json.loads(checked_list_str)
+            keywords_list = []
+
+            for keyword in checked_list:
+                if keyword:
+                    keyword_obj, created = Keyword.objects.get_or_create(word=keyword)
+                    keywords_list.append(keyword_obj)
+
+            diary.save()
+            diary.keywords.set(keywords_list)
+
+            return redirect("diary:index")
+    else:
+        form = DiaryForm(request.user)
+    context = {"form": form}
+    return render(request, "diary/diary_result_form.html", context)
 
 
 @login_required(login_url="account:login")
