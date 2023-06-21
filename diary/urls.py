0a59352d187fd3bffd2533b55cac5ad469f0e9fc
lwy210 <leeoo6436210@gmail.com>
2023-06-21 10:47:01 +0900

    feat: 일기 수정 기능 구현

--- diary/urls.py
+++ diary/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path("<int:diary_id>/", base_views.detail, name="detail"),
     path("create/", diary_views.diary_create_before, name="diary_create_before"),
     path("create/result/", diary_views.diary_create, name="diary_create"),
+    path("modify/<int:diary_id>/", diary_views.diary_modify, name="diary_modify"),
     path("bookmark/", diary_views.diary_bookmark, name="diary_bookmark_base"),
     path("bookmark/<int:diary_id>/", diary_views.diary_bookmark, name="diary_bookmark"),
 ]
