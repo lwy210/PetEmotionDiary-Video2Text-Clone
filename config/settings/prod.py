5aa34c979aae418b463a85a6d20f53cd7d65de6b
kawksung <alstjd5479@naver.com>
2023-06-23 15:20:44 +0900

    deploy: 서버환경에서만, S3 클라우드 사용하도록 변경

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -20,3 +20,16 @@ DATABASES = {
         "PORT": "3306",
     }
 }
+
+AWS_ACCESS_KEY_ID = env("S3_KEY")
+AWS_SECRET_ACCESS_KEY = env("S3_SECRET_KEY")
+AWS_REGION = "ap-northeast-2"
+
+AWS_STORAGE_BUCKET_NAME = "ped-bucket1"
+AWS_S3_CUSTOM_DOMAIN = "%s.s3.%s.amazonaws.com" % (AWS_STORAGE_BUCKET_NAME, AWS_REGION)
+
+STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/static/"
+MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/media/"
+
+DEFAULT_FILE_STORAGE = "config.settings.storages.S3DefaultStorage"
+STATICFILES_STORAGE = "config.settings.storages.S3StaticStorage"
