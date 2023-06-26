075294a8524367a13defe091cbcfcbf40b58ea64
lwy210 <leeoo6436210@gmail.com>
2023-06-26 10:52:34 +0900

    feat: 일기목록 조회 시 Calendar View 구현

--- diary/urls.py
+++ diary/urls.py
@@ -1,12 +1,18 @@
 from django.urls import path
 
-from .views import base_views, diary_views
+from .views import ajax_views, base_views, diary_views
 
 app_name = "diary"
 
 urlpatterns = [
     path("", base_views.index, name="index"),
     path("<int:diary_id>/", base_views.detail, name="detail"),
+    path("get_diarys/", ajax_views.get_diarys, name="get_diarys"),
+    path(
+        "get_diary_info/<int:diary_id>/",
+        ajax_views.get_diary_info,
+        name="get_diary_info",
+    ),
     path("create/", diary_views.diary_create_before, name="diary_create_before"),
     path("create/result/", diary_views.diary_create, name="diary_create"),
     path("modify/<int:diary_id>/", diary_views.diary_modify, name="diary_modify"),
